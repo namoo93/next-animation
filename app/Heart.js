611c6c51b79f7cd9e778d81c390be71f92970fb0
HyoSean <kimhyosun@gmail.com>
2023-07-13 20:09:56 +0900

    add normal balloon css

--- app/Heart.js
+++ app/Heart.js
@@ -6,6 +6,7 @@ export default function HeartComponent() {
         <div className='heart_balloon_left'></div>
         <div className='heart_balloon_bottom'></div>
       </div>
+      <div className='balloon_highlight'></div>
       <div className='balloon_bottom_wrap'>
         <div className='balloon_bottom'></div>
       </div>
--- app/Normal.js
+++ app/Normal.js
@@ -1,7 +1,9 @@
 export default function NormalComponent() {
   return (
-    <div className='balloon_flower balloon_animation'>
-      <div className='flower_head'></div>
+    <div className='balloon_normal balloon_animation'>
+      <div className='normal_head'>
+        <div className='balloon_highlight'></div>
+      </div>
       <div className='balloon_bottom'></div>
     </div>
   );
