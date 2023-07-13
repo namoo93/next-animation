63d7d46699e0805e20d910b0ca4be7217dc27ee8
HyoSean <kimhyosun@gmail.com>
2023-07-14 01:26:36 +0900

    풍선 인터랙션요소 추가
    풍선이 집 위치에서 생성시 위로 움직이는 css 요소 추가

--- app/BalloonWrap.js
+++ app/BalloonWrap.js
@@ -1,11 +1,17 @@
+import { useEffect, useState } from 'react';
+
 export default function BalloonWrapComponent({ children, translateX, translateY, scale, rotate, handleDelete, value }) {
+  const [fixed, setFixed] = useState(true);
+  useEffect(() => {
+    setFixed(false);
+  }, []);
   return (
     <div
       style={{
-        transform: `translate(${translateX}, ${translateY}) scale(${scale}) rotate(${rotate}deg) `,
+        transform: `translateX(${translateX}) translateY(${translateY}) scale(${scale}) rotate(${rotate}deg) `,
       }}
       onClick={() => handleDelete(value)}
-      className='balloon_wrap'>
+      className={`balloon_wrap ${fixed && 'fixed'}`}>
       {children}
     </div>
   );
